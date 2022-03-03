e3d5a053d5a0a359de93e519cdac81b2c5b69810
Marco Mariani <marco@crowdsec.net>
2022-03-03 11:22:12 +0100

    -

--- tests/lib/assert-crowdsec-not-running.sh
+++ tests/lib/assert-crowdsec-not-running.sh
@@ -1,4 +1,5 @@
 if pgrep crowdsec >/dev/null; then
+    # removing this causes CI to fail sometimes
     sleep 2
     if pgrep crowdsec >/dev/null; then
        echo "A CrowdSec process is already running. Please terminate it and run tests again." >&3
